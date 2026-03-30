52b9a4b5659b77d0babbe4a2d2515eb03ce5bf0d
LAPTOP-TANPRLPA\huil3 <huil33@naver.com>
2026-03-30 15:05:39 +0900

    enum클래스 추가 Getter/Setter 추가 CurrentWeapon 값에 따른 myWeapon이름 변경

--- 20Group-TeamProject/Player.h
+++ 20Group-TeamProject/Player.h
@@ -5,19 +5,31 @@
 class Player : public Character
 {
 private:
+	enum WeaponType
+	{
+		non,
+		knife,
+		pistol,
+		shotgun,
+
+	};
 	int level;
 	int maxLevel;
 	int exp;
 	int expToNextLevel;
 	int gold;
-	std::string weapon;
+	WeaponType currentWeapon;
+	std::string myWeapon;
 	bool pistolIsArmed;
 	bool shotgunIsArmed;
 public:
 	Player();
 
 	~Player();
-
+	void ArmedWeapon();
+	WeaponType GetArmedWeapon();
+	void SetArmedWeapon(int value);
+	void WeaponName();
 	void LevelUp();
 	void GainExp(int amount);
 	void Heal(int value);
@@ -26,5 +38,5 @@ public:
 	void ShowStatus() const;
 	void ResetNickName();
 	void GetRest();
-	void WeaponType();
+	void CurrentWeaponType();
 };
